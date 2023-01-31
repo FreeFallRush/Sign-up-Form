90ff0265069f95c684c7a3962c7d33954901ae59
FreeFallRush <queenofcups.altbot@gmail.com>
2023-02-01 00:54:24 +0200

    Updated main.js
    added event listener for pass icons to make pass visible / hidden

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const passIcon = document.querySelectorAll(".show-hide");
+
+//SHOW / HIDE PASSWORD WHEN THE ICON IS CLICKED
+passIcon.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const passInput = icon.parentElement.querySelector("input");
+
+    if (passInput.type === "password") {
+      icon.classList.replace("bx-hide", "bx-show");
+      return (passInput.type = "text");
+    }
+    icon.classList.replace("bx-show", "bx-hide");
+    passInput.type = "password";
+  });
+});
